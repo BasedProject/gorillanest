8774974b2acdf9a8c6a6c5064eb58b10fbfe79cb
anon <anon@anon.anon>
2025-10-30 23:10:35 +0100

    +dummy_repositories

---
+++ dummy_repositories/bootstrap.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+mkdir -p anon
+cd anon
+
+git clone --bare https://github.com/agvxov/cursed_c
+git clone --bare https://github.com/agvxov/dictate
+git clone --bare https://github.com/agvxov/chadsay
