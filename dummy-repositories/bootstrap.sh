825d8de6cbf4b3fa0f8319affc5ab25610eb13d1
anon <anon@anon.anon>
2026-03-13 18:56:28 +0100

    rename dummy-repositories; add empty test repo to bootstrap.sh

--- dummy-repositories/bootstrap.sh
+++ dummy-repositories/bootstrap.sh
@@ -6,3 +6,8 @@ cd anon
 git clone --bare https://github.com/agvxov/cursed_c
 git clone --bare https://github.com/agvxov/dictate
 git clone --bare https://github.com/agvxov/chadsay
+
+mkdir test
+cd test
+git init --bare
+cd ..
